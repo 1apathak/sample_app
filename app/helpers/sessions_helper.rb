98656339cd01d551bb2429b2535e5f743ba63e8b
1apathak@gmail.com <1apathak@gmail.com>
2013-10-20 17:07:18 -0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,10 @@ def sign_in(user)
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -23,6 +27,16 @@ def sign_in(user)
    def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
