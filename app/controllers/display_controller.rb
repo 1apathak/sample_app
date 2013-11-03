5c0a409e539caf830ce0772983d24bdde06e61b9
1apathak@gmail.com <1apathak@gmail.com>
2013-11-02 20:29:14 -0700

    checked for duplates, adjusted update link

--- app/controllers/display_controller.rb
+++ app/controllers/display_controller.rb
@@ -30,17 +30,27 @@ def update
 	#@youtubeString = 'http://www.youtube.com/watch?v=' + params[:vid]
 	@mymicropost = Micropost.find_by(content: 'http://www.youtube.com/watch?v=' + params[:vid]) #get the micropost
 	
-	#if the duration of the video isn't listed, get it from the youtube api
-	if @mymicropost.duration==0
-		requestString = 'https://gdata.youtube.com/feeds/api/videos/' + params[:vid] + '?v=2'
-		@doc = Nokogiri::XML(open(requestString))
-		@duration = ActionController::Base.helpers.strip_tags(@doc.xpath('//yt:duration').attr("seconds").text)
-		@duration = @duration.to_i
-		@mymicropost.duration = @duration
+	if @mymicropost.present?
+		#if the duration of the video isn't listed, get it from the youtube api
+		if @mymicropost.duration==0
+			requestString = 'https://gdata.youtube.com/feeds/api/videos/' + params[:vid] + '?v=2'
+			@doc = Nokogiri::XML(open(requestString))
+			@duration = ActionController::Base.helpers.strip_tags(@doc.xpath('//yt:duration').attr("seconds").text)
+			@duration = @duration.to_i
+			@mymicropost.duration = @duration
+		end
+
+		@mymicropost.lastwatch = DateTime.now
+		@mymicropost.quality += params[:like].to_i
+		@mymicropost.secondswatched += params[:duration].to_i
+		@mymicropost.timesplayed += 1
+
+		@mymicropost.save
+		@success=1
+	else
+		@success=0
 	end
 
-	@mymicropost.lastwatch = DateTime.now
-	@mymicropost.save
 	@displayVid=1
 end
 
